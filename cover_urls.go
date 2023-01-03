8a02eb9b909db039b34ce950c9ab029f406a40e3
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2023-01-02 20:51:03 -0800

    Expanding cover urls to support more sizes and hosts

---
+++ cover_urls.go
@@ -0,0 +1,48 @@
+package litres_integration
+
+import (
+	"math"
+	"net/url"
+	"strconv"
+	"strings"
+)
+
+type CoverSize int
+
+const (
+	Size330 CoverSize = 330
+	Size415 CoverSize = 415
+	SizeMax CoverSize = math.MaxInt
+)
+
+func coverSizeUrl(id int64, host string, coverSizePath string) *url.URL {
+	cp := strings.Replace(coverPathTemplate, "{id}", strconv.FormatInt(id, 10), -1)
+
+	return &url.URL{
+		Scheme: scheme,
+		Host:   host,
+		Path:   coverSizePath + cp,
+	}
+}
+
+func pathSize(size CoverSize) string {
+	path := coverPath
+	switch size {
+	case Size330:
+		path = cover330Path
+	case Size415:
+		path = cover415Path
+	case SizeMax:
+		// do nothing - that's the default value
+	}
+
+	return path
+}
+
+func CoverUrl(id int64, size CoverSize) *url.URL {
+	return coverSizeUrl(id, cvLitResHost, pathSize(size))
+}
+
+func PartnerCoverUrl(id int64, size CoverSize) *url.URL {
+	return coverSizeUrl(id, partnersDownloadHost, pathSize(size))
+}
--- urls.go
+++ urls.go
@@ -9,14 +9,14 @@ import (
 const (
 	scheme               = "https"
 	LitResHost           = "litres.ru"
+	cvLitResHost         = "cv." + LitResHost
 	wwwHost              = "www." + LitResHost
 	partnersDownloadHost = "partnersdnld." + LitResHost
 
 	coverPath        = "/pub/c/cover"
 	cover330Path     = "/pub/c/cover_330"
+	cover415Path     = "/pub/c/cover_415"
 	myBooksFreshPath = "/pages/my_books_fresh"
-	detailedDataPath = "/static/ds/" + DetailedDataFilename
-	biblioBookPath   = "/pages/biblio_book/"
 
 	coverPathTemplate = "/{id}.jpg"
 	pagePathTemplate  = "/page-{num}"
@@ -41,22 +41,4 @@ func HrefUrl(href string) *url.URL {
 		Host:   wwwHost,
 		Path:   href,
 	}
-}
-
-func coverSizeUrl(id int64, coverSizePath string) *url.URL {
-	cp := strings.Replace(coverPathTemplate, "{id}", strconv.FormatInt(id, 10), -1)
-
-	return &url.URL{
-		Scheme: scheme,
-		Host:   partnersDownloadHost,
-		Path:   coverSizePath + cp,
-	}
-}
-
-func CoverUrl(id int64) *url.URL {
-	return coverSizeUrl(id, coverPath)
-}
-
-func Cover330Url(id int64) *url.URL {
-	return coverSizeUrl(id, cover330Path)
 }
