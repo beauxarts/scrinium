46280b9b90cfdf582760113b60903eacc0973edf
Bogdan Brinza <Bogdan.Brinza@hey.com>
2024-11-07 14:27:27 -0800

    Adding ArtType helpers to replace gnarly conversions in fedorov

--- litres_integration/art_type.go
+++ litres_integration/art_type.go
@@ -4,6 +4,25 @@ type ArtType int
 
 const (
 	ArtTypeText  ArtType = 0
-	ArtTypePDF   ArtType = 4
 	ArtTypeAudio ArtType = 1
+	ArtTypePDF   ArtType = 4
 )
+
+var artTypeStrings = map[ArtType]string{
+	ArtTypeText:  "Текст",
+	ArtTypeAudio: "Аудио",
+	ArtTypePDF:   "PDF",
+}
+
+func (at ArtType) String() string {
+	return artTypeStrings[at]
+}
+
+func ParseArtType(ats string) ArtType {
+	for at, str := range artTypeStrings {
+		if str == ats {
+			return at
+		}
+	}
+	return ArtTypeText
+}
